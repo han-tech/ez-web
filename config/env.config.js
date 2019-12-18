9eac9e9166fb84f33f142d82462c2614753b061a
Son Dinh <peaceman77@hotmail.com>
2019-12-18 10:29:56 +0700

    Initializing code

--- config/env.config.js
+++ config/env.config.js
@@ -3,6 +3,6 @@ module.exports = {
     "environment": "dev",
     "mongoDbUri": process.env.MONGODB_URI || "mongodb://localhost/github-consumer",
     "githubEndpoint": "https://api.github.com/graphql",
-    "githubAccessToken": process.env.GITHUB_ACCESS_TOKEN,
+    "githubAccessToken": process.env.GITHUB_ACCESS_TOKEN || "6fc20d916685695fa33eb68eb5e632121d9b195f",
     "netlifyEndpoint": process.env.NETLIFY_BUILD_HOOK || ""
 };
