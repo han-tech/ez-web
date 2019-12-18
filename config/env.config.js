901f3979f17e9b3aca436e91d020cf0876427332
Son Dinh <peaceman77@hotmail.com>
2019-12-18 10:59:00 +0700

    add hooking for building

--- config/env.config.js
+++ config/env.config.js
@@ -4,5 +4,5 @@ module.exports = {
     "mongoDbUri": process.env.MONGODB_URI || "mongodb://localhost/github-consumer",
     "githubEndpoint": "https://api.github.com/graphql",
     "githubAccessToken": process.env.GITHUB_ACCESS_TOKEN || "6fc20d916685695fa33eb68eb5e632121d9b195f",
-    "netlifyEndpoint": process.env.NETLIFY_BUILD_HOOK || ""
+    "netlifyEndpoint": process.env.NETLIFY_BUILD_HOOK || "https://api.netlify.com/build_hooks/5df9a398d9cbaf38acea6775"
 };
