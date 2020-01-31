c910b8bdbd6b4a5d348cdeb32faf45c9377c0dc6
Han tech <33927705+han-tech@users.noreply.github.com>
2020-01-31 15:10:51 +0700

    add create webhook for buttercms

--- services/buttercmsService.js
+++ services/buttercmsService.js
@@ -2,6 +2,23 @@ const Config = require('../config/env.config');
 var FormData = require('form-data');
 const butterCMSApiUrl = Config.butterCMSApiUrl;
 const axios = require('axios');
+const
+  path = require('path'),
+  _ = require('underscore')
+;
+var
+  // close env to pass to spawn
+  env = _.clone(process.env),
+  // get the PATH - in my local Windows, it was Path
+  envPath = env.PATH || env.Path,
+  // get path to node_modules folder where casperjs and
+  // phantomjs-prebuilt are installed
+  // this will be different for you
+  binDir = path.join(__dirname, './../node_modules/.bin'),
+  Spooky = require('spooky')
+;
+// update the path in the cloned env
+env.PATH = env.Path = `${envPath};${binDir}`;
 class ButterCMSService{
     async createPage(butterCMSWriteApiKey, page){
         const url = butterCMSApiUrl+'pages/';
@@ -33,19 +50,111 @@ class ButterCMSService{
         };
         return await axios.post(url, content, config);
     }
-    async createWebHook(butterCMSUsername,butterCMSPassword, webhook){
-        const url = 'https://buttercms.com/login';
-        var bodyFormData = new FormData();
-        bodyFormData.append('username', butterCMSUsername);
-        bodyFormData.append('password', butterCMSPassword);
-        const headers = {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        };
-        axios({method:'post',url:url, data:bodyFormData, headers:bodyFormData.getHeaders()}).then(result => {
-            // Handle result…
-            console.log(result.data);
-          });
-        return result;
+    async createWebHook(butterCMSUsername, butterCMSPassword, webhookUrl){
+        var spooky = new Spooky({
+            child: {
+                // spooky is trying to call casperjs.bat for windows
+                // .bat doesn't work, so call the update .cmd file
+                // this fixes issue 2 with the file
+                command: /^win/.test(process.platform) ? 'casperjs.cmd' : 'casperjs',
+                transport: 'http' ,
+                spawnOptions: {
+                    // set the env using cloned version
+                    // this fixes issue 1 with the path
+                    env: env
+                }
+            },
+            casper: {
+                logLevel: 'debug',
+                verbose: true,
+                pageSettings: {
+                        loadImages:  false,         // The WebPage instance used by Casper will
+                        loadPlugins: false,         // use these settings
+                        userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.4 (KHTML, like Gecko) Chrome/22.0.1229.94 Safari/537.4'
+                }
+            }
+        }, function (err) {
+            if (err) {
+                e = new Error('Failed to initialize SpookyJS');
+                e.details = err;
+                throw e;
+            }
+            
+            var url = 'https://buttercms.com/login/?next=/webhooks/';
+            spooky.start(url);
+            spooky.then([{username: butterCMSUsername, password:butterCMSPassword, url:webhookUrl},function () {
+                this.fill('form.form-horizontal', { 
+                    username: username, 
+                    password:  password
+                }, true);
+                this.waitFor(
+                    function check() {
+                        return (this.getCurrentUrl() === 'https://buttercms.com/webhooks/');
+                    },
+                    function then() { // step to execute when check() is ok
+                        this.fill('form[method="post"]', { 
+                            'form-0-target': url, 
+                            'form-0-event':  'post.all'
+                        }, true);
+                        this.waitFor(
+                            function check() {
+                                return this.evaluate(function() {
+                                    return document.querySelector('select#id_form-0-event').selectedIndex === 1; 
+                                });
+                            },
+                            function then() { // step to execute when check() is ok
+                                this.fill('form[method="post"]', { 
+                                    'form-1-target': url, 
+                                    'form-1-event':  'page.all'
+                                }, true);
+                                this.waitFor(
+                                    function check() {
+                                        return this.evaluate(function() {
+                                            return document.querySelector('select#id_form-1-event').selectedIndex === 6; 
+                                        });
+                                    },
+                                    function then() { // step to execute when check() is ok
+                                        this.fill('form[method="post"]', { 
+                                            'form-2-target': url, 
+                                            'form-2-event':  'contentfield.update'
+                                        }, true);
+                                    },
+                                    function timeout() { // step to execute if check has failed
+                                        e = new Error('Failed to create webhook for all pages');
+                                        e.details = err;
+                                        throw e;                            
+                                    }
+                                );  
+                            },
+                            function timeout() { // step to execute if check has failed
+                                e = new Error('Failed to create webhook for all posts');
+                                e.details = err;
+                                throw e;                            
+                            }
+                        );  
+                    },
+                    function timeout() { // step to execute if check has failed
+                        e = new Error('Failed to navigate to home');
+                            e.details = err;
+                            throw e; 
+                    }
+                );   
+            }]);
+            spooky.run();
+        });
+        spooky.on('error', function (e, stack) {
+            console.error(e);
+        
+            if (stack) {
+                console.log(stack);
+            }
+        });
+        spooky.on('log', function (log) {
+            if (log.space === 'remote') {
+                console.log(log.message.replace(/ \- .*/, ''));
+            }
+        });
+        return null;
     }
 }
 exports.ButterCMSService = ButterCMSService;
--- services/test.js
+++ services/test.js
@@ -1,50 +1,10 @@
-var casper = require('casper').create({   
-    verbose: true, 
-    logLevel: 'debug',
-    pageSettings: {
-         loadImages:  false,         // The WebPage instance used by Casper will
-         loadPlugins: false,         // use these settings
-         userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.4 (KHTML, like Gecko) Chrome/22.0.1229.94 Safari/537.4'
+const ButterCMSService = require('../services/butterCMSService').ButterCMSService;
+const butterCMSService = new ButterCMSService();
+(async () => {
+    try {
+        const result = butterCMSService.createWebHook('dinhquangson@gmail.com','Th@nhb1nh', 'https://api.zeit.co/v1/integrations/deploy/QmSy4kGHQsJ6jbBJb7jJniLwsbfL9X3yXnCN4YsugXEGFx/jtlWuXPtVq');
+    } catch (e) {
+        // Deal with the fact the chain failed
+        console.log(e);
     }
-});
-phantom.casperTest = true;
-// print out all the messages in the headless browser context
-casper.on('remote.message', function(msg) {
-    this.echo('remote message caught: ' + msg);
-});
-
-// print out all the messages in the headless browser context
-casper.on("page.error", function(msg, trace) {
-    this.echo("Page Error: " + msg, "ERROR");
-});
-var url = 'https://buttercms.com/login/?next=/webhooks/';
-
-casper.start(url, function() {
-   console.log("page loaded");
-   this.test.assertExists('form.form-horizontal', 'form is found');
-   this.fill('form.form-horizontal', { 
-        username: 'dinhquangson@gmail.com', 
-        password:  'Th@nhb1nh'
-    }, true);
-});
-
-casper.then(function(){
-    this.waitFor(function check() {
-        return (this.getCurrentUrl() === 'https://buttercms.com/webhooks/');
-    },
-    function then() { // step to execute when check() is ok
-        this.echo('Navigated to home', 'INFO');
-        this.fill('form[method="post"]', { 
-            'form-0-target': 'https://api.zeit.co/v1/integrations/deploy/QmSy4kGHQsJ6jbBJb7jJniLwsbfL9X3yXnCN4YsugXEGFx/jtlWuXPtVq', 
-            'form-0-event':  'post.all'
-        }, true);
-    },
-    function timeout() { // step to execute if check has failed
-        this.echo('Failed to navigate to home', 'ERROR');
-    });   
-});
-
-casper.run(function() {
-    this.echo('Done');
-    this.exit();            
-});
+})();
