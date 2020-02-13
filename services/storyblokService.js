4559caf2e0b06a43039de3ce0b21c190f6c392a2
Dinh Son <dinhquangson@users.noreply.github.com>
2020-02-13 18:35:59 +0700

    Update storyblok

--- services/storyblokService.js
+++ services/storyblokService.js
@@ -3,37 +3,37 @@ const storyblokApiUrl = Config.storyblokApiUrl;
 const axios = require('axios');
 class StoryblokService{
     async createComponent(storyblokManagementApiKey, spaceId, component){
-        const url = storyblokManagementApiKey+spaceId+'/components/';
+        const url = storyblokApiUrl+spaceId+'/components/';
         const config = {
             headers : {
                 'Content-Type': 'application/json',
-                'Authorization':'Bearer '+kontentManagementApiKey
+                'Authorization':'Bearer '+storyblokManagementApiKey
             }
         };
         return await axios.post(url, component, config);
     }
     
     async createAndPublishContent(storyblokManagementApiKey, spaceId, content, variant){
-        const itemsUrl = kontentApiUrl+project+'/items/';
+        const itemsUrl = storyblokApiUrl+spaceId+'/items/';
         const config = {
             headers : {
                 'Content-Type': 'application/json',
-                'Authorization':'Bearer '+kontentManagementApiKey
+                'Authorization':'Bearer '+storyblokManagementApiKey
             }
         };
         const item = await axios.post(itemsUrl, content, config);
-        const variantsUrl = kontentApiUrl+project+'/items/'+item.data.id+'/variants/00000000-0000-0000-0000-000000000000';
+        const variantsUrl = storyblokApiUrl+spaceId+'/items/'+item.data.id+'/variants/00000000-0000-0000-0000-000000000000';
         
         var result = await axios.put(variantsUrl, variant, config);
         const publishUrl = variantsUrl + '/publish';
         return await axios.put(publishUrl, null, config);
     }
-    async createWebHook(kontentManagementApiKey, project, webhook){
-        const webhooksUrl = kontentApiUrl+project+'/webhooks/';
+    async createWebHook(storyblokManagementApiKey, spaceId, webhook){
+        const webhooksUrl = storyblokApiUrl+spaceId+'/webhooks/';
         const config = {
             headers : {
                 'Content-Type': 'application/json',
-                'Authorization':'Bearer '+kontentManagementApiKey
+                'Authorization':'Bearer '+storyblokManagementApiKey
             }
         };
         
