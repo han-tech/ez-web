2326acb8e4d669fb24de6879c96f02c1ae8cd9e1
Dinh Son <dinhquangson@users.noreply.github.com>
2020-02-14 18:09:48 +0700

    Support storyblok

--- services/storyblokService.js
+++ services/storyblokService.js
@@ -1,43 +1,52 @@
-const Config = require('../config/env.config');
-const storyblokApiUrl = Config.storyblokApiUrl;
-const axios = require('axios');
+// npm install storyblok-js-client
+const StoryblokClient = require('storyblok-js-client')
+
+// Initialize the client with the oauth token
+
 class StoryblokService{
-    async createComponent(storyblokManagementApiKey, spaceId, component){
-        const url = storyblokApiUrl+spaceId+'/components/';
-        const config = {
-            headers : {
-                'Content-Type': 'application/json',
-                'Authorization':'Bearer '+storyblokManagementApiKey
-            }
-        };
-        return await axios.post(url, component, config);
+    async createComponentAndStories(storyblokManagementApiKey, spaceId, stories, component){
+        let Storyblok = new StoryblokClient({
+            oauthToken: storyblokManagementApiKey,
+            cache: {type: 'memory',clear: 'auto'}
+        });
+
+        stories.forEach(story => {
+            Storyblok.post(`spaces/${spaceId}/stories/`, {
+                story
+            }).then(res => {
+                console.log(`Success: ${res.data.story.name} was created.`)
+            }).catch(err => {
+                console.log(`Error: ${err}`)
+            });
+        });
+
+        Storyblok.post(`spaces/${spaceId}/components/`, component).then(res => {
+            console.log(`Success: ${res.data.component.name} was created.`)
+        }).catch(err => {
+            console.log(`Error: ${err}`)
+        });
+        
+        return null;
     }
     
-    async createAndPublishContent(storyblokManagementApiKey, spaceId, content, variant){
-        const itemsUrl = storyblokApiUrl+spaceId+'/items/';
-        const config = {
-            headers : {
-                'Content-Type': 'application/json',
-                'Authorization':'Bearer '+storyblokManagementApiKey
-            }
-        };
-        const item = await axios.post(itemsUrl, content, config);
-        const variantsUrl = storyblokApiUrl+spaceId+'/items/'+item.data.id+'/variants/00000000-0000-0000-0000-000000000000';
-        
-        var result = await axios.put(variantsUrl, variant, config);
-        const publishUrl = variantsUrl + '/publish';
-        return await axios.put(publishUrl, null, config);
-    }
     async createWebHook(storyblokManagementApiKey, spaceId, webhook){
-        const webhooksUrl = storyblokApiUrl+spaceId+'/webhooks/';
-        const config = {
-            headers : {
-                'Content-Type': 'application/json',
-                'Authorization':'Bearer '+storyblokManagementApiKey
-            }
-        };
+        let Storyblok = new StoryblokClient({
+            oauthToken: storyblokManagementApiKey,
+            cache: {type: 'memory',clear: 'auto'}
+        });
+        Storyblok.get(`spaces/${spaceId}/`).then(res => {
+            var space = res.data;
+            space.story_published_hook = webhook.story_published_hook;
+            Storyblok.put(`spaces/${spaceId}/`, space).then(res => {
+                console.log(`Success: ${res.data.space.story_published_hook} was created.`)
+            }).catch(err => {
+                console.log(`Error: ${err}`)
+            });
+        }).catch(err => {
+            console.log(`Error: ${err}`)
+        });        
         
-        return await axios.post(webhooksUrl, webhook, config);
+        return null;
     }
 }
 exports.StoryblokService = StoryblokService;
--- services/test.js
+++ services/test.js
@@ -1,8 +1,53 @@
-const ForestryService = require('../services/forestryService').ForestryService;
-const forestryService = new ForestryService();
+const StoryblokService = require('../services/storyblokService').StoryblokService;
+const storyblokService = new StoryblokService();
 (async () => {
     try {
-        const site = forestryService.addSite('dinhquangson@gmail.com', 'Th@nh+b1nh', 'han-tech/brevifolia-gatsby-forestry','https://www.google.com');
+        // one line of csv in here
+        let story = {
+            slug: 'abcdefss',
+            name: 'Greata',
+            parent_id: null,
+            content: {
+                component: 'post2',
+                title: 'That\'s was great',
+                text: 'body is here',
+                image: 'https://a.storyblok.com/f/51376/x/19b4879a86/logo.svg',
+                category: 'category 2'
+            }
+        };
+        let stories=[story];
+        // Done reading the CSV - now we finally create the component with a definition for each field
+        // we can also skip that and define the content type using the interface at app.storyblok.com
+        let component = {
+            "component": {
+                name: "post2",
+                display_name: "Post",
+                schema: {
+                    title: {
+                        type: "text",
+                        pos: 0
+                    },
+                    text: {
+                        type: "markdown",
+                        pos: 1
+                    },
+                    image: {
+                        type: "image",
+                        pos: 2
+                    },
+                    category: {
+                        type: "text",
+                        pos: 3
+                    }
+                },
+                is_root: true, // is content type
+                is_nestable: false // is nestable (in another content type)
+            }
+        }
+        const webhook = {
+            story_published_hook: "https://webhook.site/d99e2bd5-5761-4db5-8889-3f6e99cb06c9"
+        };
+        const site = await storyblokService.createWebHook('bSYBR6Il3gqBYHMb8nNoPgtt-60420-DYpQs8xCekru6GT2xeLR', '75501', webhook);
         console.log(site);
     } catch (e) {
         // Deal with the fact the chain failed
